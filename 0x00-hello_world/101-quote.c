d0ede45b052622ad3bbcad9fed732947869390a2
kenyapower <vincentn371@gmail.com>
2023-02-16 10:59:17 -0800

    program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
