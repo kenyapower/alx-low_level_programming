8d658a3de7da584b4611c92c1a8db4ba42446413
kenyapower <vincentn371@gmail.com>
2023-02-17 10:29:13 -0800

    program that prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Start point of program
- * Description - program that prints the alphabet in lowercase, followed by a new line
+ * Description - prints the alphabet in lowercase, followed by a new line
  *
  * Return: Always 0 (Success)
  */
