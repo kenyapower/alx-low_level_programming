57f8ede71db8b5a6aa933f89ef2dbe2055f6697f
kenyapower <vincentn371@gmail.com>
2023-02-21 03:43:18 -0800

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description - program that prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char k;
+
+	for (k = 'A'; k <= 'Z'; k++)
+	{
+		putchar(k);
+	}
+
+	return (0);
+}
