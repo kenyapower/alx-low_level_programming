a375c3dfefa984e8b8d2d49392cc1c03431e5c02
kenyapower <vincentn371@gmail.com>
2023-02-21 05:31:37 -0800

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Desc - prints the alphabet, in lowercase
+*
+* Return: Always 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
