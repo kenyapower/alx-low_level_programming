f27c1ec37b42c0b24c2022bc11b48109a7c30dad
kenyapower <vincentn371@gmail.com>
2023-02-23 11:36:48 -0800

    4-print_nost numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,18 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9 &  a new line
- * do not print 2 & 4
- * use ascii referencing
- *
- * Return: nothing
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
  */
-
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = '48'; i <= '57'; i++)
+	for (i = 48; i < 58; ch++)
 	{
-		if (i != '50' && i != '52')
-		{
+		if (i != 50 && i != 52)
 			_putchar(i);
-		}
-
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
