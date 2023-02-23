5c59a07dfe1eb18590c171155885911923284b10
kenyapower <vincentn371@gmail.com>
2023-02-23 11:20:44 -0800

    main.h file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef main.h
-#define main.h
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,5 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif
