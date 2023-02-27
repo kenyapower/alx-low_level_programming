114ee2646f665bffe6fd2defc900f4e6b17cd413
kenyapower <vincentn371@gmail.com>
2023-02-27 12:05:19 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,13 @@
 
 void print_rev(char *s)
 {
-	int count = 0;
+	int len = 0;
 
-	while (count >= 0)
-	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
+	while (s[len] != '\0')
+		len++;
 
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
+	while (len)
+		_putchar(s[--len]);
+
 	_putchar('\n');
 }
