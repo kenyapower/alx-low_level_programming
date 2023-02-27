2dfaedd3ee76da2ea6ab07f2543abfeb88692857
kenyapower <vincentn371@gmail.com>
2023-02-27 11:30:00 -0800

    function that copies the string pointed to by src

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef main.h
-#define main.h
-
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,5 +8,3 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
