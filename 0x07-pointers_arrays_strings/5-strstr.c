e21f71ba8fdac67170e3b1eab8a2f5f092a59cd5
kenyapower <vincentn371@gmail.com>
2023-03-06 07:57:36 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 #include <stdio.h>
 
 /**
@@ -9,6 +8,7 @@
  * Return:  a pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
  */
+
 char *_strstr(char *haystack, char *needle)
 {
 	char *startn = needle, *starth = haystack;
